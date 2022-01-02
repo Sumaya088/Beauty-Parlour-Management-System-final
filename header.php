d9fc5ab463adbf5af0f37f194168d6b7d7a7fdb6
Sumaya088 <85355330+Sumaya088@users.noreply.github.com>
2022-01-02 10:23:21 +0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco_navbar ftco-navbar-light" id="ftco-navbar">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">Beauty Parlour Management System</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+       
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="services.php" class="nav-link">Services</a></li>
+            
+            <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+           
+            <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+            
+            <!-- <li class="nav-item"><a href="admin/index.php" class="nav-link">Admin</a></li> -->
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
