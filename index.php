9bfd82283732eb9dcae516015e865063b9fe8442
Sumaya088 <85355330+Sumaya088@users.noreply.github.com>
2022-01-02 10:24:27 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,123 +1,211 @@
-<?php
+<?php 
+include('includes/dbconnection.php');
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
-
-if(isset($_POST['login']))
+if(isset($_POST['submit']))
   {
-    $adminuser=$_POST['username'];
-    $password=md5($_POST['password']);
-    $query=mysqli_query($con,"select ID from tbladmin where  UserName='$adminuser' && Password='$password' ");
-    $ret=mysqli_fetch_array($query);
-    if($ret>0){
-      $_SESSION['bpmsaid']=$ret['ID'];
-     header('location:dashboard.php');
-    }
-    else{
-    $msg="Invalid Details.";
-    }
-  }
-  ?>
-<!DOCTYPE HTML>
-<html>
-<head>
-<title>BPMS | Login Page </title>
 
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- Bootstrap Core CSS -->
-<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
-<!-- Custom CSS -->
-<link href="css/style.css" rel='stylesheet' type='text/css' />
-<!-- font CSS -->
-<!-- font-awesome icons -->
-<link href="css/font-awesome.css" rel="stylesheet"> 
-<!-- //font-awesome icons -->
- <!-- js-->
-<script src="js/jquery-1.11.1.min.js"></script>
-<script src="js/modernizr.custom.js"></script>
-<!--webfonts-->
-<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
-<!--//webfonts--> 
-<!--animate-->
-<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
-<script src="js/wow.min.js"></script>
-	<script>
-		 new WOW().init();
-	</script>
-<!--//end-animate-->
-<!-- Metis Menu -->
-<script src="js/metisMenu.min.js"></script>
-<script src="js/custom.js"></script>
-<link href="css/custom.css" rel="stylesheet">
-<!--//Metis Menu -->
-</head> 
-<body class="cbp-spmenu-push">
-	<div class="main-content">
-		
-		<!-- main content start-->
-		<div id="page-wrapper">
-			<div class="main-page login-page ">
-				<h3 class="title1">SignIn Page</h3>
-				<div class="widget-shadow">
-					<div class="login-top">
-						<h4>Welcome back to BPMS AdminPanel ! </h4>
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $services=$_POST['services'];
+    $adate=$_POST['adate'];
+    $atime=$_POST['atime'];
+    $phone=$_POST['phone'];
+    $aptnumber = mt_rand(100000000, 999999999);
+  
+    $query=mysqli_query($con,"insert into tblappointment(AptNumber,Name,Email,PhoneNumber,AptDate,AptTime,Services) value('$aptnumber','$name','$email','$phone','$adate','$atime','$services')");
+    if ($query) {
+$ret=mysqli_query($con,"select AptNumber from tblappointment where Email='$email' and  PhoneNumber='$phone'");
+$result=mysqli_fetch_array($ret);
+$_SESSION['aptno']=$result['AptNumber'];
+ echo "<script>window.location.href='thank-you.php'</script>";	
+  }
+  else
+    {
+      $msg="Something Went Wrong. Please try again";
+    }
+
+  
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>BPMS||Home Page</title>
+        
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+
+	
+
+  </head>
+  <body>
+	  <?php include_once('includes/header.php');?>
+    <!-- END nav -->
+
+    <section id="home-section" class="hero" style="background-image: url(images/bg.jpg);" data-stellar-background-ratio="0.5">
+		  <div class="home-slider owl-carousel">
+	      <div class="slider-item js-fullheight">
+	      	<div class="overlay"></div>
+	        <div class="container-fluid p-0">
+	          <div class="row d-md-flex no-gutters slider-text align-items-end justify-content-end" data-scrollax-parent="true">
+	          	<img class="one-third align-self-end order-md-last img-fluid" src="images/bg_1.png" alt="">
+		          <div class="one-forth d-flex align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+		          	<div class="text mt-5">
+		          		<span class="subheading">Beauty Parlour</span>
+			            <h1 class="mb-4">Get Pretty Look</h1>
+			            <p class="mb-4">We pride ourselves on our high quality work and attention to detail. The products we use are of top quality branded products.</p>
+			            
+			           
+		            </div>
+		          </div>
+	        	</div>
+	        </div>
+	      </div>
+
+	      <div class="slider-item js-fullheight">
+	      	<div class="overlay"></div>
+	        <div class="container-fluid p-0">
+	          <div class="row d-flex no-gutters slider-text align-items-center justify-content-end" data-scrollax-parent="true">
+	          	<img class="one-third align-self-end order-md-last img-fluid" src="images/bg_2.png" alt="">
+		          <div class="one-forth d-flex align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+		          	<div class="text mt-5">
+		          		<span class="subheading">Natural Beauty</span>
+			            <h1 class="mb-4">Beauty Salon</h1>
+			            <p class="mb-4">This parlour provides huge facilities with advanced technology equipments and best quality service. Here we offer best treatment that you might have never experienced before.</p>
+			            
+			           
+		            </div>
+		          </div>
+	        	</div>
+	        </div>
+	      </div>
+	    </div>
+    </section>
+
+
+<br>
+    <section class="ftco-section ftco-no-pt ftco-booking">
+    	<div class="container-fluid px-0">
+    		<div class="row no-gutters d-md-flex justify-content-end">
+    			<div class="one-forth d-flex align-items-end">
+    				<div class="text">
+    					<div class="overlay"></div>
+    					<div class="appointment-wrap">
+    						<span class="subheading">Reservation</span>
+								<h3 class="mb-2">Make an Appointment</h3>
+		    				<form action="#" method="post" class="appointment-form">
+			            <div class="row">
+			              <div class="col-sm-12">
+			                <div class="form-group">
+					              <input type="text" class="form-control" id="name" placeholder="Name" name="name" required="true">
+					            </div>
+			              </div>
+			              <div class="col-sm-12">
+			                <div class="form-group">
+					              <input type="email" class="form-control" id="appointment_email" placeholder="Email" name="email" required="true">
+					            </div>
+			              </div>
+				            <div class="col-sm-12">
+			                <div class="form-group">
+					              <div class="select-wrap">
+		                      <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+		                      <select name="services" id="services" required="true" class="form-control">
+		                      	<option value="">Select Services</option>
+		                      	<?php $query=mysqli_query($con,"select * from tblservices");
+              while($row=mysqli_fetch_array($query))
+              {
+              ?>
+		                       <option value="<?php echo $row['ServiceName'];?>"><?php echo $row['ServiceName'];?></option>
+		                       <?php } ?> 
+		                      </select>
+		                    </div>
+					            </div>
+			              </div>
+			              <div class="col-sm-12">
+			                <div class="form-group">
+			                  <input type="text" class="form-control appointment_date" placeholder="Date" name="adate" id='adate' required="true">
+			                </div>    
+			              </div>
+			              <div class="col-sm-12">
+			                <div class="form-group">
+			                  <input type="text" class="form-control appointment_time" placeholder="Time" name="atime" id='atime' required="true">
+			                </div>
+			              </div>
+			              <div class="col-sm-12">
+			                <div class="form-group">
+			                  <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone" required="true" maxlength="10" pattern="[0-9]+">
+			                </div>
+			              </div>
+				          </div>
+				          <div class="form-group">
+			              <input type="submit" name="submit" value="Make an Appointment" class="btn btn-primary">
+			            </div>
+			          </form>
+		          </div>
+						</div>
+    			</div>
+					<div class="one-third">
+						<div class="img" style="background-image: url(images/bg-1.jpg);">
+						</div>
 					</div>
-					<div class="login-body">
-						<form role="form" method="post" action="">
-							<p style="font-size:16px; color:red" align="center"> <?php if($msg){
-    echo $msg;
-  }  ?> </p>
-							<input type="text" class="user" name="username" placeholder="Username" required="true">
-							<input type="password" name="password" class="lock" placeholder="Password" required="true">
-							<input type="submit" name="login" value="Sign In">
-							<div class="forgot-grid">
-								
-								<div class="forgot">
-									<a href="../index.php">Back to Home</a>
-								</div>
-								<div class="clearfix"> </div>
-							</div>
-							<div class="forgot-grid">
-								
-								<div class="forgot">
-									<a href="forgot-password.php">forgot password?</a>
-								</div>
-								<div class="clearfix"> </div>
-							</div>
-						</form>
-					</div>
-				</div>
-				
-				
-			</div>
-		</div>
+    		</div>
+    	</div>
+    </section>
+
 		
-	</div>
-	<!-- Classie -->
-		<script src="js/classie.js"></script>
-		<script>
-			var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
-				showLeftPush = document.getElementById( 'showLeftPush' ),
-				body = document.body;
-				
-			showLeftPush.onclick = function() {
-				classie.toggle( this, 'active' );
-				classie.toggle( body, 'cbp-spmenu-push-toright' );
-				classie.toggle( menuLeft, 'cbp-spmenu-open' );
-				disableOther( 'showLeftPush' );
-			};
-			
-			function disableOther( button ) {
-				if( button !== 'showLeftPush' ) {
-					classie.toggle( showLeftPush, 'disabled' );
-				}
-			}
-		</script>
-	<!--scrolling js-->
-	<script src="js/jquery.nicescroll.js"></script>
-	<script src="js/scripts.js"></script>
-	<!--//scrolling js-->
-	<!-- Bootstrap Core JavaScript -->
-   <script src="js/bootstrap.js"> </script>
-</body>
+		<br>
+
+
+   <?php include_once('includes/footer.php');?>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
 </html>
